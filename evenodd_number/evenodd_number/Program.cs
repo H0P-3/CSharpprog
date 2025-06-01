fbe6019811479fce48b5af50c310963cd68c34bb
unknown <hopeoluwasegun2017@gmail.com>
2025-06-01 23:14:27 +0100

    Add EvenOdd program

---
+++ evenodd_number/evenodd_number/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+
+namespace evenodd_number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Enter the Number: ");
+            string input = Console.ReadLine();
+            if (int.TryParse(input, out int number))
+            {
+                if (number % 2 == 0)
+                {
+                    Console.WriteLine("Your number is even");
+                }
+                else
+                {
+                    Console.WriteLine("Your number is odd");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a valid integer.");
+            }
+            Console.ReadLine();
+
+
+           
+        }
+    }
+}
